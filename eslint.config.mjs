a2a276bd5c09d0642b7857e4f645adf7f8e60256
Andrés Martínez <autummata@gmail.com>
2025-02-05 20:14:48 -0600

    Update: Eslint Config to include sarif export

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,22 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import js from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { languageOptions: { globals: globals.browser } },
+  js.configs.recommended,
+  {
+    ignores: ["node_modules/**"],
+  },
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+  },
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
